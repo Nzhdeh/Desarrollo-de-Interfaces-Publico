62a25fa12a86fab0b0813d2396acfa5fbd2636db
Nzhdeh <nzhdeh1991@gmail.com>
2019-10-14 13:04:46 +0200

    Modelo vista

---
+++ 09-ModeloVisatasWU/09-ModeloVisatasWU/Model/ClsPersona.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_ModeloVisatasWU.Model
+{
+   public class ClsPersona
+    {
+        //atributos privados
+        private string _nombre;//_ para todos los atributos privados
+        private string _apellidos;
+        private DateTime _fechaNacimiento;
+
+        //const por defecto
+        public ClsPersona()
+        {
+            this.nombre = "No hay nombre";
+            this.apellidos = "No hay apellidos";
+            this.fechaNacimiento = new DateTime();
+        }
+
+        //constr sobrecargado
+        public ClsPersona(String nombre, String apellidos, int dia,int mes,int anio)
+        {
+            this._nombre = nombre;
+            this._apellidos = apellidos;
+            this._fechaNacimiento = new DateTime(dia,mes,anio);
+        }
+
+        //propiedades publicas
+        public string nombre
+        {
+            get
+            {
+                return _nombre;
+            }
+            set
+            {
+                if (_nombre != "")
+                    _nombre = value;
+            }
+        }
+
+        public string apellidos
+        {
+            get
+            {
+                return _apellidos;
+            }
+            set
+            {
+                if(_apellidos!="")
+                _apellidos = value;
+            }
+        }
+
+        public DateTime fechaNacimiento
+        {
+            get
+            {
+                return _fechaNacimiento;
+            }
+            set
+            {
+                _fechaNacimiento = value;
+            }
+        }
+    }
+}
