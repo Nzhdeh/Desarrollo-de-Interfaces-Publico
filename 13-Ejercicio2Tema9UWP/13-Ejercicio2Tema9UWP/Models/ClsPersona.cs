b57495769ff7f4f8d2d7a163069f43de6363ed6c
nzhdeh21 <nzhdeh1991@gmail.com>
2019-10-25 00:30:07 +0200

    kfhd

---
+++ 13-Ejercicio2Tema9UWP/13-Ejercicio2Tema9UWP/Models/ClsPersona.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_Ejercicio2Tema9UWP.Models
+{
+   public class ClsPersona : INotifyPropertyChange
+    {
+        //atributos privados
+        private string _nombre;//_ para todos los atributos privados
+        //private string _apellidos;
+        //private DateTime _fechaNacimiento;
+
+        //const por defecto
+        public ClsPersona()
+        {
+            this.nombre = "No hay nombre";
+            //this.apellidos = "No hay apellidos";
+            //this.fechaNacimiento = new DateTime();
+        }
+
+        //constr sobrecargado
+        public ClsPersona(String nombre)
+        {
+            this._nombre = nombre;
+            //this._apellidos = apellidos;
+            //this._fechaNacimiento = new DateTime(dia,mes,anio);
+        }
+
+        //propiedades publicas
+        public string nombre
+        {
+            get
+            {
+                return _nombre;
+            }
+            set
+            {
+                if (_nombre != "")
+                    _nombre = value;
+            }
+        }
+
+        //public string apellidos
+        //{
+        //    get
+        //    {
+        //        return _apellidos;
+        //    }
+        //    set
+        //    {
+        //        if(_apellidos!="")
+        //        _apellidos = value;
+        //    }
+        //}
+
+        //public DateTime fechaNacimiento
+        //{
+        //    get
+        //    {
+        //        return _fechaNacimiento;
+        //    }
+        //    set
+        //    {
+        //        _fechaNacimiento = value;
+        //    }
+        //}
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    /// <summary>
+    /// no funcionaaaaa
+    /// </summary>
+    public interface INotifyPropertyChange
+    {
+
+    }
+
+    /// <summary>
+    /// PropertyChanged no funciona
+    /// </summary>
+    /// <param name="propertyName"></param>
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        if (PropertyChanged != null)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
