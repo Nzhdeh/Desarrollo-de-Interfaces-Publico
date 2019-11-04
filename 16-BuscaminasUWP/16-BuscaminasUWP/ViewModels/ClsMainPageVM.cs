04ceb8dcd09e7108fd92c1822b40c9e76b20c0b0
Nzhdeh <nzhdeh1991@gmail.com>
2019-11-04 14:37:00 +0100

    f450

--- 16-BuscaminasUWP/16-BuscaminasUWP/ViewModels/ClsMainPageVM.cs
+++ 16-BuscaminasUWP/16-BuscaminasUWP/ViewModels/ClsMainPageVM.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using _16_BuscaminasUWP.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _16_BuscaminasUWP.ViewModels
 {
-    class ClsMainPageVM
+    public class ClsMainPageVM : INotifyPropertyChanged
     {
+        //atributos privados
+        private ClsCasilla casillaSeleccionada;
+        private List<ClsCasilla> tablero;
+        private int contAciertos;
+        private String notificarGanadorPerdedor;
+
+
+
+        //atributos publicos
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
