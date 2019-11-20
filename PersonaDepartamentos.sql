4d87abe929e482756d6f8b0f1e036627486e73ae
Nzhdeh <nzhdeh1991@gmail.com>
2019-11-20 12:38:09 +0100

    hjj

--- PersonaDepartamentos.sql
+++ PersonaDepartamentos.sql
@@ -1,9 +1,9 @@
-use Personas
+use personasDB2
 go
 
 create table PD_Departamentos
 (
-	IdDepartamento int identity(1,1) not null,
+	IdDepartamento int not null,
 	NombreDepartamento varchar (20) null,
 
 	constraint PK_PD_Departamentos primary key (IdDepartamento)
@@ -12,17 +12,18 @@ go
 
 create table PD_Personas
 (
-	IdPersona int identity(1,1) not null,
+	IdPersona int not null,
 	NombrePersona varchar (20) null,
 	ApellidosPersona varchar (30) null,
 	IDDepartamento int null,
 	FechaNacimientoPersona date null,
 	TelefonoPersona varchar (15) null,
 	FotoPersona varbinary(max) null,
+	Direccion varchar(40) null
 
 	constraint PK_PD_Personas primary key (IdPersona),
 	constraint FK_Persona_Departamento foreign key (IDDepartamento) 
-	references PD_Departamentos(IdDepartamento) on delete no action on update cascade
+	references PD_Departamentos(IdDepartamento)
 )
 
 --drop database PersonaDepartamento
