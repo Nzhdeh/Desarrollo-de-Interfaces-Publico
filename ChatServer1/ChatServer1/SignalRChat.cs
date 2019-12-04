c8a8113d0f448ab3ea5ccd444af76bece4801adb
Nzhdeh <nzhdeh1991@gmail.com>
2019-12-04 14:34:51 +0100

    terminarlo

---
+++ ChatServer1/ChatServer1/SignalRChat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace ChatServer1
+{
+    public class SignalRChat : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+
+        public void SendUWP(ClsMensage msg)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(msg);
+        }
+    }
+}
