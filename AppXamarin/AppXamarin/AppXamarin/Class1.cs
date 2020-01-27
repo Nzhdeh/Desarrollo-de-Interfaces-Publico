73ecdeb36c445b53bf68de668f538f66900caa4b
Nzhdeh <nzhdeh1991@gmail.com>
2020-01-27 14:40:17 +0100

    xamarin

---
+++ AppXamarin/AppXamarin/AppXamarin/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace AppXamarin
+{
+    public class Class1 : ClsVMBase
+    {
+        private string name;
+
+        public ICommand EnterCommand => new Command(EnterCommandExecute);
+
+        
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                NotifyPropertyChanged("Name");
+            }
+        }
+
+        private async void EnterCommandExecute()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                await Application.Current.MainPage.DisplayAlert("Alert", "Alerta mannn", "OK");
+                return;
+            }
+
+            await Application.Current.MainPage.Navigation.PushAsync(new SecondPage());
+        }
+    }
+}
