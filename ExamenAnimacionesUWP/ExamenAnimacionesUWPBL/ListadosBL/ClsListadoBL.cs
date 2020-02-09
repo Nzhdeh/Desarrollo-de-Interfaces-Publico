309bdfcfecd2e957b0f2b3c9601b543b93dc85f5
nzhdeh21 <nzhdeh1991@gmail.com>
2020-02-09 22:57:32 +0100

    dgrfreg

--- ExamenAnimacionesUWP/ExamenAnimacionesUWPBL/ListadosBL/ClsListadoBL.cs
+++ ExamenAnimacionesUWP/ExamenAnimacionesUWPBL/ListadosBL/ClsListadoBL.cs
@@ -8,6 +8,16 @@ namespace ExamenAnimacionesUWPBL.ListadosBL
 {
     public class ClsListadoBL
     {
-
+        /// <summary>
+        /// Recoge de la capa DAL el listado y lo pasa a la CAPA UI
+        /// </summary>
+        /// <returns>El listado de personas List<clsPersona></returns>
+        //public async Task<List<clsPersona>> listadoPersonas()
+        //{
+        //    clsListadoPersonasDAL listBBDD = new clsListadoPersonasDAL();
+        //    Task<List<clsPersona>> l = listBBDD.listadoPersona();
+        //    List<clsPersona> listado = await listBBDD.listadoPersona();
+        //    return listado;
+        //}
     }
 }
