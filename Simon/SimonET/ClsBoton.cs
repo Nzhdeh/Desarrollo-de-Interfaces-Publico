82fc244543af3631e063089f27b706f1fe44a1aa
Nzhdeh <nzhdeh1991@gmail.com>
2020-02-19 12:51:13 +0100

    grt

---
+++ Simon/SimonET/ClsBoton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimonET
+{
+    public class ClsBoton
+    {
+        private int id;
+        private string sonido;
+        private string color;
+
+        public ClsBoton()
+        {
+            this.id = 0;
+            this.sonido = "";
+            this.color = "";
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Sonido
+        {
+            get { return sonido; }
+            set { sonido = value; }
+        }
+
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+    }
+}
